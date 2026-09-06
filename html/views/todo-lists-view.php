276c7e133ea809bc4e6e8ee5a74f345fcc80ee6d
DylanCheetah <dylan_the_cheetah@outlook.com>
2026-09-06 18:12:12 -0400

    Correct todo list detail path in PHP file as well.

--- html/views/todo-lists-view.php
+++ html/views/todo-lists-view.php
@@ -14,7 +14,7 @@
             <div class="row m-1">
                 <div class="col card bg-white">
                     <div class="card-body row">
-                        <a class="col-9 nav-link" href="/todo-lists/<?php echo $todoList['id']; ?>">
+                        <a class="col-9 nav-link" href="/todo-lists/<?php echo $todoList['id']; ?>/">
                             <?php echo $todoList['name']; ?></a>
                         <form class="col-3" hx-post="/todo-lists/<?php echo $todoList['id']; ?>/delete/"
                             hx-indicator="#todoListDeleteBtn<?php $todoList['id']; ?>">
