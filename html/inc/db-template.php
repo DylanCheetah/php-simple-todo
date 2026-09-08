b8ea27641e6e3c1e7f18d158592c0a74ddde279f
DylanCheetah <dylan_the_cheetah@outlook.com>
2026-09-08 00:13:23 -0400

    Replace database credentials with template file.

--- html/inc/db-template.php
+++ html/inc/db-template.php
@@ -3,6 +3,6 @@
 $db = new PDO(
     'mysql:host=127.0.0.1;dbname=php_simple_todo', 
     'php_simple_todo', 
-    'e21ea297103b85cba1530c148e08a10f65a30ef2ed100e873f49d6d5e53d78ab'
+    'YOUR-PASSWORD-HERE'
 );
 ?>
