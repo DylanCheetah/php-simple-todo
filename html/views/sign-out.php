ef6e60bcab9df2460ae87a6827dbcfcdf4caab42
DylanCheetah <dylan_the_cheetah@outlook.com>
2026-09-12 17:50:14 -0400

    Fix spelling on sign-out page.

--- html/views/sign-out.php
+++ html/views/sign-out.php
@@ -18,13 +18,13 @@ if($method === 'POST') {
 }
 
 // Send page header
-$viewName = 'Sign-Out';
+$viewName = 'Sign Out';
 require_once(__DIR__ . '/../inc/page-header.php');
 ?>
 <div class="row justify-content-center">
     <div class="col-6 m-2 card bg-light">
         <div class="card-body">
-            <h1>Sign-Out</h1>
+            <h1>Sign Out</h1>
             <hr/>
             <form method="POST">
                 <input type="hidden" name="csrfToken" value="<?php echo csrfToken(); ?>"/>
@@ -32,7 +32,7 @@ require_once(__DIR__ . '/../inc/page-header.php');
                     <p class="col">Are you sure you want to sign out?</p>
                 </div>
                 <div class="row justify-content-end m-2 mt-4">
-                    <button class="col-2 btn btn-primary">Sign-Out</button>
+                    <button class="col-2 btn btn-primary">Sign Out</button>
                 </div>
             </form>
         </div>
