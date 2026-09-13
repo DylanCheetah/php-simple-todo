9d67e6b4eb2f2a48c722aa67e46842e88c384170
DylanCheetah <dylan_the_cheetah@outlook.com>
2026-09-13 01:39:39 -0400

    Fix todo list update form bug.

--- html/views/todo-list-update.php
+++ html/views/todo-list-update.php
@@ -23,7 +23,7 @@ if($method === 'POST') {
         // Update the todo list
         if(updateTodoList($objectId, $name)) {
             // Redirect to the details page
-            header("Location: /todo-lists/$objectId/");
+            header("Location: /todo-lists/$objectId/info/");
             exit();
         }
 
